ff79afcca38a59627ff8674dc31a8465371bb2ff
Ricardo Manzano <ricardoamsrams@gmail.com>
2024-10-31 10:53:32 -0400

    Add component to the app main component app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageCropComponent } from './image-crop/image-crop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ImageCropComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
