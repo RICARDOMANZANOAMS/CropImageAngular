7e31042414f90279e1f2ff3a5a428877feed0c7c
Ricardo Manzano <ricardoamsrams@gmail.com>
2024-10-31 10:46:13 -0400

    Import modules to the image-crop component

--- src/app/image-crop/image-crop.component.ts
+++ src/app/image-crop/image-crop.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageCropperComponent } from 'ngx-image-cropper';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-image-crop',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ImageCropperComponent],
   templateUrl: './image-crop.component.html',
   styleUrl: './image-crop.component.css'
 })
