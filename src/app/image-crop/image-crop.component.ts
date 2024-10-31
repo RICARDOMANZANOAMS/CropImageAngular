716bd4accc26fd52a6d66c4c2eef93b428a09a0b
Ricardo Manzano <ricardoamsrams@gmail.com>
2024-10-31 10:34:44 -0400

    Add component inside the app

---
+++ src/app/image-crop/image-crop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-crop',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-crop.component.html',
+  styleUrl: './image-crop.component.css'
+})
+export class ImageCropComponent {
+
+}
